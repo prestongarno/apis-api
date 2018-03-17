e82591abdbad9457e3e29cc8c6a5d3de24484fca
Preston Garno <prestongarno@gmail.com>
2018-03-17 13:09:17 -0400

    Added AWS ECS coord

--- deploy/docker-aws-push.sh
+++ deploy/docker-aws-push.sh
@@ -2,6 +2,7 @@
 
 exit 0
 
+#788626906849.dkr.ecr.us-east-1.amazonaws.com/apis-api
 if [ "$BRANCH" == "master" ]; then
   docker --version  # document the version travis is using
   pip install --user awscli # install aws cli w/o sudo
@@ -9,8 +10,9 @@ if [ "$BRANCH" == "master" ]; then
   eval $(aws ecr get-login --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
   FULL_NAME=$PROJECT_NAME:$BUILD_VERSION
   docker build -t $PROJECT_NAME .
-  docker tag $FULL_NAME [your_ecr_account].dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
-  docker push [your_ecr_account].dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
+  docker tag $FULL_NAME 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
+  docker push 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
 else 
   echo "Not master branch, skipping AWS deploy"
 fi
+
