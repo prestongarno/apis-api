adb18974809079fc1dbccc2fa31b974679d509e7
Preston Garno <prestongarno@gmail.com>
2018-03-17 16:31:36 -0400

    nit

--- deploy/docker-ecs.sh
+++ deploy/docker-ecs.sh
@@ -12,12 +12,11 @@ if [ "$BRANCH" == "master" ]; then
   REGION="us-east-1"
   echo "aws_access_key_id sha256sum = $(echo $aws_access_key_id | sha256sum)"
 
-  docker --version  # document the version travis is using
+  docker --version
   if [[ "$(whereis aws)" = *":" ]]; then pip install --user awscli; fi
-  export PATH="$PATH:$HOME/.local/bin" # put aws in the path
-  eval $(aws ecr get-login --no-include-email --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-  #FULL_NAME=$(echo "$PROJECT_NAME:$BUILD_VERSION" | sed -E 's/(.*)/\L\1/g')
-  FULL_NAME="$PROJECT_NAME:$BUILD_VERSION"
+  export PATH="$PATH:$HOME/.local/bin"
+  eval $(aws ecr get-login --no-include-email --region us-east-1)
+  FULL_NAME="apis-api:1.0-SNAPSHOT"
   echo "FULL_NAME=$FULL_NAME"
   docker tag $FULL_NAME 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
   docker images
