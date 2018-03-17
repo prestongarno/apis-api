c5352ea68bdb6d36d68aa5250b00f0479bd9cd01
Preston Garno <prestongarno@gmail.com>
2018-03-17 14:44:39 -0400

    Fixed garbage aws python scripted cli

--- deploy/docker-ecs.sh
+++ deploy/docker-ecs.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-exit 0
-
-#788626906849.dkr.ecr.us-east-1.amazonaws.com/apis-api
 if [ "$BRANCH" == "master" ]; then
+
+  ./gradlew dockerBuildImage
 
   gpg --decrypt --batch --passphrase "$GMAIL_GPG" deploy/aws.credentials.gpg \
     | sed -E 's/^\[/#\[/g' \
     | sed -E 's/^(.*)\s=\s/\U\1=/g' \
     | source /dev/stdin
 
+  REGION="us-east-1"
+  echo "AWS_ACCESS_KEY_ID sha256sum = $(echo $AWS_ACCESS_KEY_ID | sha256sum)"
+
   docker --version  # document the version travis is using
-  pip install --user awscli # install aws cli w/o sudo
-  export PATH=$PATH:$HOME/.local/bin # put aws in the path
-  eval $(aws ecr get-login --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
-  FULL_NAME=$PROJECT_NAME:$BUILD_VERSION
-  docker build -t $PROJECT_NAME .
-  docker tag $FULL_NAME 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
-  docker push 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
+  if [[ "$(whereis aws)" = *": " ]]; then
+    pip install --user awscli # install aws cli w/o sudo
+  fi
+  export PATH="$PATH:$HOME/.local/bin" # put aws in the path
+  echo "FUCK"
+  eval $(aws ecr get-login --no-include-email --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+  echo "FUCK2"
+  FULL_NAME="$PROJECT_NAME:$BUILD_VERSION"
+  docker tag "$FULL_NAME" "788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME"
+  docker push "788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME"
 
 else 
   echo "Not master branch, skipping AWS deploy"
