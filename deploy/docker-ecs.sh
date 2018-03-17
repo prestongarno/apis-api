c27f8a27f897ed78ebb58440403b00d4659a6802
Preston Garno <prestongarno@gmail.com>
2018-03-17 15:57:32 -0400

    nit

--- deploy/docker-ecs.sh
+++ deploy/docker-ecs.sh
@@ -13,14 +13,15 @@ if [ "$BRANCH" == "master" ]; then
   echo "aws_access_key_id sha256sum = $(echo $aws_access_key_id | sha256sum)"
 
   docker --version  # document the version travis is using
-  if [[ "$(whereis aws)" = *":" ]]; then
-    pip install --user awscli # install aws cli w/o sudo
-  fi
+  if [[ "$(whereis aws)" = *":" ]]; then pip install --user awscli; fi
   export PATH="$PATH:$HOME/.local/bin" # put aws in the path
   eval $(aws ecr get-login --no-include-email --region us-east-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+  #FULL_NAME=$(echo "$PROJECT_NAME:$BUILD_VERSION" | sed -E 's/(.*)/\L\1/g')
   FULL_NAME="$PROJECT_NAME:$BUILD_VERSION"
-  docker tag "$FULL_NAME" "788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME"
-  docker push "788626906849.dkr.ecr.us-east-1.amazonaws.com/$PROJECT_NAME"
+  echo "FULL_NAME=$FULL_NAME"
+  sudo docker tag $FULL_NAME 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
+  sudo docker images
+  sudo docker push 788626906849.dkr.ecr.us-east-1.amazonaws.com/$FULL_NAME
 
 else 
   echo "Not master branch, skipping AWS deploy"
